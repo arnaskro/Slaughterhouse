b176644ae0e59e3fce83f931b166462628196926
Arnas Kromelis <arnas.kromelis@gmail.com>
2016-04-20 09:03:47 +0200

    ApplicationServer initial

---
+++ src/servers/ApplicationServer.java
@@ -0,0 +1,12 @@
+package servers;
+
+/**
+ * Created by arnas on 4/20/2016.
+ */
+public class ApplicationServer {
+
+    public static void main(String args[]) {
+        System.out.println("Application Server Test");
+    }
+
+}
