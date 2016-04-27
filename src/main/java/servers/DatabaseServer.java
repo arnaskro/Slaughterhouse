267dcb0ab6ca67db0218ac0627f430ee676f9b5f
Arnas Kromelis <arnas.kromelis@gmail.com>
2016-04-27 08:53:19 +0200

    changed structure

--- src/main/java/servers/DatabaseServer.java
+++ src/main/java/servers/DatabaseServer.java
@@ -5,20 +5,76 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
+import mediator.DatabaseAdapter;
+import mediator.Persistence;
 import model.*;
 import servers.DatabaseServerInterface;
 
 @SuppressWarnings("serial")
 public class DatabaseServer extends UnicastRemoteObject implements DatabaseServerInterface
 {
-
+	private Persistence database;
+	private ArrayList<Animal> animals;
+	private ArrayList<Part> parts;
+	private ArrayList<Tray> trays;
+	private ArrayList<Product> products;
+	private ArrayList<Market> markets;
+	
 	protected DatabaseServer() throws RemoteException 
 	{
-		super();
-		// TODO Auto-generated constructor stub
+		
+		try {
+			database = new DatabaseAdapter();
+			
+			animals = database.loadAnimals();
+			parts = database.loadParts();
+			trays = database.loadTrays();
+			products = database.loadProducts();
+			markets = database.loadMarkets();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		
+		
+		printDatabase();
+	
 	}
-
+	
+	private void printDatabase()
+	{
+		System.out.println("Animals:");
+		for (int i = 0; i < animals.size(); i++) {
+			System.out.println(
+					animals.get(i).getWeight() + " " + animals.get(i).getAnimalId() + " " + animals.get(i).getType());
+		}
+		System.out.println("Parts:");
+		for (int i = 0; i < parts.size(); i++) {
+			System.out
+					.println(parts.get(i).getPartId() + " " + parts.get(i).getType() + " " + parts.get(i).getWeight());
+		}
+		System.out.println("Trays:");
+		for (int i = 0; i < trays.size(); i++) {
+			System.out.println(
+					trays.get(i).getType() + " " + trays.get(i).getTrayId() + " " + trays.get(i).getMaxWeight());
+			System.out.println("Trays Parts:");
+			for (int j = 0; j < trays.get(i).getParts().size(); j++) {
+				System.out.println("     " + trays.get(i).getParts().get(j).getPartId() + " "
+						+ trays.get(i).getParts().get(j).getType() + " " + trays.get(i).getParts().get(j).getWeight());
+			}
+		}
+		System.out.println("Products:");
+		for (int i = 0; i < products.size(); i++) {
+			System.out.println(products.get(i).getProductId() + " " + products.get(i).getType() + " "
+					+ products.get(i).getWeight());
+		}
+		System.out.println("Markets:");
+		for (int i = 0; i < markets.size(); i++) {
+			System.out.println(markets.get(i).getMarketId() + " " + markets.get(i).getAddress());
+		}
+	}
+	
 	public boolean addAnimal(float weight, String type) throws RemoteException 
 	{
 		// TODO Auto-generated method stub
